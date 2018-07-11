00993a86a293e3ce6eb5e8c4d22a3dd5f176e676
Aiilu <40794899+Aiilu@users.noreply.github.com>
2018-07-11 14:36:59 -0300

    Ejercicio 4/9/10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	//Genero el número RANDOM entre 1 y 10
+	
+	
+	var numero;
+	
+	numero = parseInt( Math.random()*(11-1)+1);
+
+	if( numero > 8){
+		alert("Excelente");
+	}
+	else{}
+		if ( numero > 4){
+			alert("Aprobado");
+			}
+			else{}
+				if ( numero < 4){
+				alert("Vamos, la proxima se puede!");
+				}
+			
+	
 	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,9 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var formIngreso;
+	formIngreso=parseInt(formIngreso);
+	formIngreso=Math.floor(Math.random()* (11-1) + 1);
+	alert(formIngreso);
 
 }//FIN DE LA FUNCIÓN
