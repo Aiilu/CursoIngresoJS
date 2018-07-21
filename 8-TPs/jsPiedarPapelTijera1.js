b0acf13ea01d1a78f16adce15c296793072cad83
Aiilu <40794899+Aiilu@users.noreply.github.com>
2018-07-21 16:08:35 -0300

    Piedra,papel y tijera

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,34 +5,99 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
+var piedra;
+var papel;
+var tijera;
+var edad;
+contadorPersonas;
+contadorPersonas=0;
 
-{ 
+
 
 function comenzar()
+{ 
+    eleccionMaquina = Math.floor( Math.random ()*(3-1))+1;
+    console.log(eleccionMaquina);
+  
 
-	var numeroRandom = Math.floor((Math.random ()*3-1)+1);
-    alert(numeroRandom);
-console.log("piedra");
-console.log("papel");
-console.log("tijera");
+edad = prompt("Ingrese su edad");
+
+if (edad>=18){
+document.getElementById("contadorPersonas").value=contadorPersonas;
+}
+
+    if (edad<18){
+        document.getElementById("contadorPersonas").value=contadorPersonas;
+}
+     
+    
 	
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-    alert(1);
-	
+    piedra = 1;
+    console.log(piedra);
+
+    if (piedra=eleccionMaquina){
+        alert("usted empató");
+    }
+
+    if (piedra>eleccionMaquina){
+        alert("Usted ganó");
+    }
+
+    if (piedra<eleccionMaquina){
+        alert("Usted perdió");
+    }
+
+   
+
+   
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-    alert(2);
+    papel = 2;
+    console.log(papel);
+
+    if (papel=eleccionMaquina){
+        alert("Usted empató");
+    }
+
+    if (papel>eleccionMaquina){
+        alert("Usted ganó");
+    }
+
+    if (papel<eleccionMaquina){
+        alert("Usted perdió");
+    }
+  
+   
+    
 
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	alert (3);
+    tijera = 3;
+    console.log(tijera);
+
+    if (tijera=eleccionMaquina){
+        alert("Usted empató");
+    }
+
+    if (tijera>eleccionMaquina){
+        alert("Usted ganó");
+    }
+    
+    if (tijera<eleccionMaquina){
+        alert("Usted perdió");
+    }
+   
+    
+  
+
 
 }//FIN DE LA FUNCIÓN
