9e8e181e97a32bda7785ea6bf3e05d5cbacb1305
Aiilu <ailentorrez26@gmail.com>
2018-08-01 21:57:13 -0300

    Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,63 +1,95 @@
 function mostrar()
 {
-    var num;
+    //no me da la letra maxima
     var letra;
-    var cantCeros=0;
-    var cantPares=0;
+    var num;
+    var cont=0;
+    var resp="si";
+    var cantPar=0;
     var cantImpar=0;
+    var cantCeros=0;
+    var promPosit;
     var acumPosit=0;
-    var promedPosit=0;
     var acumNeg=0;
+    var flag=true;
+    var max;
+    var min;
+    var letraMax;
+    var letraMin;
+
+    while (resp=="si")
+     {
+       cont ++;
+
+       letra = prompt ("Ingrese una letra");
+
+       num = prompt ("Ingrese un numero");
+       num = parseInt (num);
+
+       while ( num<-100 || num>100 || isNaN(num))
+        {
+            num = prompt ("Ingrese un numero");
+            num = parseInt (num);
+        }
+        
+        resp = prompt ("Quiere continuar?");  //verificacion
+
+        if (num%2==0 && num !=0) {
+            cantPar ++;                                            //a
+        }
+
+        else if (num%2!=0 && num !=0) {
+            cantImpar ++;                                          //b
+     }
+
+        else  {
+            cantCeros ++;                                          //c
+        }
+        
+        if (num>0) {
+            acumPosit+=num;                                          //d
+        }
+
+        else {
+             acumNeg+=num;                                            //e
+        }
+
+        if(flag) {
+
+            flag = false;
+            max = num;
+            min = num;
+        }
+        
+        if (num>max){
+            max = num;
+            letraMax = letra;
+        }
+
+        if (num<min) {
+            min = num;
+            letraMin = letra;
+        }
+
+        
+        
+    }
+
+    promPosit = acumPosit/cont;
+
+        document.write("Cantidad de N° Pares:" + cantPar + "<br>");
+        document.write("Cantidad de N° Impares:" + cantImpar + "<br>");
+        document.write("Cantidad de Ceros:" + cantCeros + "<br>");
+        document.write("Promedio N° Positivos:" + promPosit + "<br>");
+        document.write("Suma de N° Negativos:" + acumNeg + "<br>");
+        document.write("N° Maximo: " + max + " y Letra Maxima: " + letraMax + "<br>");
+        document.write("N° Minimo: " + min + " y Letra Minima: " + letraMin + "<br>");
+
+
+    
     
 
 
-    for (cont=0; cont<1; cont ++)
-    {
-
-      letra = prompt ("Ingrese una letra");
-
-      num = prompt ("Ingrese un numero");
-      num = parseInt (num);
-
-
-      if (num <-100 || num >100) {
-          num = prompt ("Ingrese un numero");
-          num = parseInt (num);                    //validacion
-      }
-
-      if (num == 0) {
-          cantCeros ++;                             //c
-    }
-
-    else if (num % 2 == 0) {
-        cantPares ++;                                //a
-    }
-
-    else {
-        cantImpar ++;                                 //b
-    }
-
-    if (num >0 && num<100) {
-        acumPosit ++;                                 //d
-    }
-
-    else  { 
-        acumNeg ++;                                    //e
     }
 
 
-
-
-
-
-}
-
-promedPosit = acumPosit/cont;
-
-document.write("Cantidad de Ceros:" + cantCeros + "<br>");
-document.write("Cantidad de N° Pares:" + cantPares + "<br>");
-document.write("Cantidad de N° Impares:" + cantImpar + "<br>");
-document.write("Promedio N° Positivos:" + promedPosit + "<br>");
-document.write("Suma de los negativos:" + acumNeg + "<br>" );
-
-}
