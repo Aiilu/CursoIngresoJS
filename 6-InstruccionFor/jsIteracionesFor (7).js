d06572d49f8e1188e0b426a7303ded7aad59b2a9
Aiilu <ailentorrez26@gmail.com>
2018-08-01 23:46:17 -0300

    For

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,18 @@
 function mostrar()
 {
+    var cantDiv=0;
+
+    num=prompt("ingrese un numero");
+    num=parseInt(num);
+    for(var num; num>1; num--)
+    {
+        if(num%1==0 & num !=0)
+        {
+            document.write("numeros divisores: " + num + "<br>");
+            cantDiv ++;
+        }
+    }
+    document.write("Cantidad de divisores:" + cantDiv + "<br>");
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,5 +1,12 @@
 function mostrar()
 {
+    num = prompt ("Ingrese un numero");
+    num = parseInt (num);
+
+    for (var num; num=1; num ++)
+     {
+        if (num%1==0)
+     }
 
 
 
